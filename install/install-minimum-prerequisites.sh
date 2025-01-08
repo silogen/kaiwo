54a76aeebc9772597798765c5315ffc77eb673bb
Antti-Ville Suni <anttsuni@amd.com>
2025-01-08 19:01:52 +0000

    fix installation instructions with script

--- install/install-minimum-prerequisites.sh
+++ install/install-minimum-prerequisites.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-set -e
 
 # Assuming no tweaking is needed for the cert-manager installation
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml
 
-# AMD device plugin
-kubectl create -f https://raw.githubusercontent.com/ROCm/k8s-device-plugin/master/k8s-ds-amdgpu-dp.yaml
+# AMD Operator
+helm repo add amd-gpu-operator https://rocm.github.io/gpu-operator 
+helm repo update 
+helm install amd-gpu-operator amd-gpu-operator/gpu-operator-charts --namespace kube-amd-gpu --create-namespace 
+kubectl apply -f ./deviceconfig.yaml
 
-# AMD node labeller
-kubectl create -f https://raw.githubusercontent.com/ROCm/k8s-device-plugin/master/k8s-ds-amdgpu-labeller.yaml
+# Prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts && helm repo update 
+helm --namespace prometheus-system install prometheus prometheus-community/kube-prometheus-stack --create-namespace --version 48.2.1 -f ./prometheus/prometheus-overrides.yaml 
+kubectl apply -f ./prometheus/podMonitor.yaml 
+kubectl apply -f ./prometheus/prometheusRules.yaml
+
--- install/install-ray-kueue-operators.sh
+++ install/install-ray-kueue-operators.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 
 # Install Ray Operator
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
-helm install kuberay-operator kuberay/kuberay-operator --version 1.2.2
+helm install kuberay-operator kuberay/kuberay-operator --version 1.2.2 -n default
 
 # Install Kueue Operator
 kubectl apply --server-side -f ./kueue-install.yaml
