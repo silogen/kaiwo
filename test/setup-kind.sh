f1d4052df3333af2b09fdab169cd4b51dfde646a
AV Suni <avesuni@outlook.com>
2025-02-25 16:25:21 +0200

    PR review changes

--- test/setup-kind.sh
+++ test/setup-kind.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-RECREATE="true"
 TEST_NAME=${TEST_NAME:-"kaiwo-test"}
 
 CERT_MANAGER_MANIFESTS="https://github.com/cert-manager/cert-manager/releases/download/v1.17.0/cert-manager.yaml"
@@ -17,18 +16,13 @@ create_cluster() {
 
 # Check if the Kind cluster exists
 if kind get clusters | grep -q "$TEST_NAME"; then
-    if [ "$RECREATE" = "true" ]; then
         echo "Deleting existing Kind cluster '$TEST_NAME'..."
         kind delete cluster --name "$TEST_NAME"
         create_cluster
-    else
-        echo "Kind cluster '$TEST_NAME' already exists. Skipping creation."
-    fi
 else
     create_cluster
 fi
 
-kubectl delete -f "$CERT_MANAGER_MANIFESTS" --ignore-not-found
 kubectl apply -f "$CERT_MANAGER_MANIFESTS"
 echo "Waiting for Cert-Manager to be deployed..."
 for deploy in cert-manager cert-manager-webhook cert-manager-cainjector; do
@@ -43,14 +37,12 @@ echo "Cert-Manager deployed."
 
 # Add Kueue
 echo "Deploying Kueue operator..."
-kubectl delete -f "$KUEUE_MANIFESTS" --ignore-not-found 
 kubectl apply --server-side -f "$KUEUE_MANIFESTS"
 echo "Waiting for Kueue to be deployed..."
 kubectl rollout status deployment/kueue-controller-manager -n kueue-system --timeout=1m
 echo "Kueue deployed."
 
 # Add KubeRay
-kubectl delete -k "$KUBERAY_MANIFESTS" --ignore-not-found
 kubectl create -k "$KUBERAY_MANIFESTS"
 kubectl rollout status deployment/kuberay-operator --timeout=1m
 echo "KubeRay deployed."
