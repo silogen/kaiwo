876e305bcadd176d75df5fd3d60b242ce9bcfa4c
Alex Saliniemi <alex.saliniemi@amd.com>
2025-03-13 11:38:15 +0000

    updated tests

---
+++ pkg/cli/tests.go
@@ -0,0 +1,98 @@
+// Copyright 2025 Advanced Micro Devices, Inc.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+
+	testutils "github.com/silogen/kaiwo/pkg/utils/test"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	e2eTestSourceDir string
+	e2eTestOutputDir string
+)
+
+func BuildTestsCmd() *cobra.Command {
+	generateCmd := &cobra.Command{
+		Use:   "generate",
+		Short: "Generate assets",
+	}
+
+	generateE2eTestsCmd := &cobra.Command{
+		Use:   "e2e-tests",
+		Short: "Generate end-to-end tests",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runGenerateE2eTests(e2eTestSourceDir, e2eTestOutputDir)
+		},
+	}
+
+	generateCmd.AddCommand(generateE2eTestsCmd)
+	generateE2eTestsCmd.Flags().StringVarP(&e2eTestSourceDir, "input", "", "", "Directory to load feature files from")
+	generateE2eTestsCmd.Flags().StringVarP(&e2eTestOutputDir, "output", "", "", "Directory to save generated tests in")
+
+	return generateCmd
+}
+
+func runGenerateE2eTests(inputDir string, outputDir string) error {
+	var features []testutils.GherkinFeature
+	fmt.Println("Generating e2e tests...")
+	fmt.Printf("Input dir: %s\n", inputDir)
+
+	err := filepath.WalkDir(inputDir, func(path string, d fs.DirEntry, err error) error {
+		fmt.Println("Walking directory:", path)
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && filepath.Ext(path) == ".yaml" && filepath.Base(path) != "" {
+			fmt.Println("Loading feature file:", path)
+			if strings.HasSuffix(path, ".feature.yaml") {
+				var feature testutils.GherkinFeature
+				data, err := os.ReadFile(path)
+				if err != nil {
+					return fmt.Errorf("could not read file %s: %w", path, err)
+				}
+
+				err = yaml.Unmarshal(data, &feature)
+				if err != nil {
+					return fmt.Errorf("could not unmarshal file %s: %w", path, err)
+				}
+				fmt.Printf("Found file: %s\n", path)
+				features = append(features, feature)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("could not walk dir: %w", err)
+	}
+
+	for _, feature := range features {
+		fmt.Printf("Building tests")
+		if err := feature.CreateChainsawTests(outputDir, inputDir); err != nil {
+			return fmt.Errorf("could not build chainsaw tests: %w", err)
+		}
+	}
+	return nil
+}
---
+++ pkg/utils/test/gherkin.go
@@ -0,0 +1,363 @@
+// Copyright 2025 Advanced Micro Devices, Inc.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testutils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+
+	baseutils "github.com/silogen/kaiwo/pkg/utils"
+
+	"github.com/imdario/mergo"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+// GherkinFeature represents a full Gherkin feature.
+type GherkinFeature struct {
+	ScenarioWrapper GherkinScenarioWrapper `yaml:",inline"`
+	Tags            []string               `yaml:"tags,omitempty"`
+	Reference       *ResourceRef           `yaml:"reference,omitempty"`
+	Rules           []Rule                 `yaml:"rules,omitempty"`
+}
+
+func (f *GherkinFeature) CreateChainsawTests(folder string, inputFolder string) error {
+	reference, err := f.Reference.Contents(inputFolder)
+	if err != nil {
+		return fmt.Errorf("failed to read gherkin reference contents: %w", err)
+	}
+	sanitizedName := baseutils.MakeRFC1123Compliant(f.ScenarioWrapper.Name)
+	featureFolder := filepath.Join(folder, sanitizedName)
+
+	if err := f.ScenarioWrapper.Build(featureFolder, reference, sanitizedName); err != nil {
+		return fmt.Errorf("failed to build gherkin feature: %w", err)
+	}
+
+	for _, rule := range f.Rules {
+		sanitizedRuleName := baseutils.MakeRFC1123Compliant(rule.ScenarioWrapper.Name)
+		ruleFolder := filepath.Join(featureFolder, "rules", sanitizedRuleName)
+		if err := rule.ScenarioWrapper.Build(ruleFolder, reference, fmt.Sprintf("%s--%s", sanitizedName, sanitizedRuleName)); err != nil {
+			return fmt.Errorf("failed to build gherkin feature rule: %w", err)
+		}
+	}
+
+	return nil
+}
+
+type ResourceRef struct {
+	File     string         `yaml:"file,omitempty"`
+	Resource map[string]any `yaml:"resource,omitempty"`
+}
+
+func (r *ResourceRef) Contents(rootDir string) (*unstructured.Unstructured, error) {
+	var reference *unstructured.Unstructured
+
+	if r != nil {
+		if r.File != "" {
+			contents, err := os.ReadFile(filepath.Join(rootDir, r.File))
+			if err != nil {
+				return nil, fmt.Errorf("failed to read file %s: %w", r.File, err)
+			}
+			var data map[string]interface{}
+			if err := yaml.Unmarshal(contents, &data); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal file %s: %w", r.File, err)
+			}
+			reference = &unstructured.Unstructured{Object: data}
+		} else if r.Resource != nil {
+			reference = &unstructured.Unstructured{Object: r.Resource}
+		}
+	}
+	return reference, nil
+}
+
+// Scenario represents a Gherkin scenario, which can modify the reference.
+type Scenario struct {
+	Name        string   `yaml:"name"`
+	Description string   `yaml:"description,omitempty"`
+	Tags        []string `yaml:"tags,omitempty"`
+	Steps       []Step   `yaml:"steps"`
+}
+
+// Step represents a Gherkin step.
+type Step struct {
+	// Gherkin fields
+	Given string `yaml:"given,omitempty"`
+	When  string `yaml:"when,omitempty"`
+	Then  string `yaml:"then,omitempty"`
+	And   string `yaml:"and,omitempty"`
+	But   string `yaml:"but,omitempty"`
+
+	Chainsaw ChainsawStep `yaml:",inline"`
+
+	// ApplyPatch includes the patch to apply on top of the current base reference
+	ApplyPatch *map[string]any `yaml:"applyPatch,omitempty"`
+}
+
+type ChainsawStep struct {
+	Apply    *map[string]interface{} `yaml:"apply,omitempty"`
+	Assert   *map[string]interface{} `yaml:"assert,omitempty"`
+	Command  *map[string]interface{} `yaml:"command,omitempty"`
+	Create   *map[string]interface{} `yaml:"create,omitempty"`
+	Delete   *map[string]interface{} `yaml:"delete,omitempty"`
+	Describe *map[string]interface{} `yaml:"describe,omitempty"`
+	Error    *map[string]interface{} `yaml:"error,omitempty"`
+	Events   *map[string]interface{} `yaml:"events,omitempty"`
+	Get      *map[string]interface{} `yaml:"get,omitempty"`
+	Patch    *map[string]interface{} `yaml:"patch,omitempty"`
+	PodLogs  *map[string]interface{} `yaml:"podLogs,omitempty"`
+	Proxy    *map[string]interface{} `yaml:"proxy,omitempty"`
+	Script   *map[string]interface{} `yaml:"script,omitempty"`
+	Sleep    *map[string]interface{} `yaml:"sleep,omitempty"`
+	Update   *map[string]interface{} `yaml:"update,omitempty"`
+	Wait     *map[string]interface{} `yaml:"wait,omitempty"`
+}
+
+func (s *Step) IsBuildStep() bool {
+	return s.Given != "" || s.When != ""
+}
+
+func (s *Step) IsAssertStep() bool {
+	return !s.IsBuildStep()
+}
+
+func (s *Step) GetName() (string, error) {
+	name := ""
+	prefix := ""
+	if s.Given != "" {
+		name = s.Given
+		prefix = "given"
+	} else if s.When != "" {
+		name = s.When
+		prefix = "when"
+	} else if s.Then != "" {
+		name = s.Then
+		prefix = "then"
+	} else if s.And != "" {
+		name = s.And
+		prefix = "and"
+	} else if s.But != "" {
+		name = s.But
+		prefix = "but"
+	} else {
+		return "", fmt.Errorf("no name for step, must give one of given, when, then, and or but")
+	}
+
+	return fmt.Sprintf("%s: %s", prefix, name), nil
+}
+
+//type ScenarioOutline struct {
+//	Tags     []string                `yaml:"tags,omitempty"`
+//	Steps    []Step                  `yaml:"steps"`
+//	Examples ScenarioOutlineExamples `yaml:"examples"`
+//}
+//
+//type ScenarioOutlineExamples struct {
+//	// Fixture allows inline defining of the test fixtures for this scenario outline
+//	Fixture Fixture `yaml:"fixture,omitempty"`
+//
+//	// Parameters lists the fixture parameters that are requested by this scenario outline
+//	Parameters []string `yaml:"parameters,omitempty"`
+//}
+
+type GherkinScenarioWrapper struct {
+	Name        string     `yaml:"name"`
+	Description string     `yaml:"description,omitempty"`
+	Background  []Step     `yaml:"background,omitempty"`
+	Scenarios   []Scenario `yaml:"scenarios"`
+}
+
+func (g GherkinScenarioWrapper) Build(folder string, baseObject *unstructured.Unstructured, nameBase string) error {
+	if baseObject != nil {
+		baseObject = baseObject.DeepCopy()
+	}
+
+	fmt.Println(baseObject)
+
+	var applyObjectKeywords []string
+
+	backgroundSteps, applyObject, keywords, err := BuildChainsawSteps(g.Background, baseObject, applyObjectKeywords)
+	if err != nil {
+		return fmt.Errorf("failed to build background steps: %w", err)
+	}
+	applyObjectKeywords = keywords
+	fmt.Println(keywords)
+	baseObject = applyObject
+	fmt.Println(baseObject)
+
+	for _, scenario := range g.Scenarios {
+
+		scenarioSteps, _, _, err := BuildChainsawSteps(scenario.Steps, baseObject, applyObjectKeywords)
+		if err != nil {
+			return fmt.Errorf("failed to build scenario steps: %w", err)
+		}
+
+		steps := append(backgroundSteps, scenarioSteps...)
+
+		chainsawTest := map[string]any{
+			"apiVersion": "chainsaw.kyverno.io/v1alpha1",
+			"kind":       "Test",
+			"metadata": map[string]any{
+				"name": fmt.Sprintf("%s--%s", nameBase, baseutils.MakeRFC1123Compliant(scenario.Name)),
+			},
+			"spec": map[string]any{
+				"steps": []map[string]any{
+					{
+						"try": steps,
+						"catch": []map[string]interface{}{
+							{"events": map[string]any{}},
+							{"podLogs": map[string]any{}},
+						},
+					},
+				},
+			},
+		}
+
+		chainsawTestYaml, err := yaml.Marshal(chainsawTest)
+		if err != nil {
+			return fmt.Errorf("failed to marshal chainsaw steps: %w", err)
+		}
+
+		scenarioFolder := filepath.Join(folder, baseutils.MakeRFC1123Compliant(scenario.Name))
+		if err := os.MkdirAll(scenarioFolder, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create gherkin scenario folder: %w", err)
+		}
+
+		if err := os.WriteFile(filepath.Join(scenarioFolder, "chainsaw-test.yaml"), chainsawTestYaml, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create gherkin scenario steps: %w", err)
+		}
+	}
+	return nil
+}
+
+func BuildChainsawSteps(gherkinSteps []Step, applyObject *unstructured.Unstructured, applyObjectKeywords []string) ([]any, *unstructured.Unstructured, []string, error) {
+	var chainsawSteps []any
+
+	buildPhase := true
+	applyObject = applyObject.DeepCopy()
+	for i, step := range gherkinSteps {
+		fmt.Println(step.GetName())
+		if step.IsAssertStep() {
+			if buildPhase && applyObject != nil {
+				// Finished the build phase (given, when) and there is an object that should be applied
+				fmt.Println("Adding", applyObject)
+				chainsawSteps = append(chainsawSteps, map[string]interface{}{
+					"apply": map[string]interface{}{
+						"reference": applyObject,
+					},
+					"description": strings.Join(applyObjectKeywords, "\n"),
+				})
+			}
+			buildPhase = false
+		} else {
+			if !buildPhase {
+				return nil, nil, nil, fmt.Errorf("cannot include a build step (given, when) after an assert step (then, and, but)")
+			}
+
+			name, err := step.GetName()
+			if err != nil {
+				return nil, nil, nil, fmt.Errorf("failed to get name for step %d: %w", i, err)
+			}
+			applyObjectKeywords = append(applyObjectKeywords, name)
+
+			if step.ApplyPatch != nil {
+				applyPatchReference := &unstructured.Unstructured{Object: *step.ApplyPatch}
+				if err := mergo.Merge(applyObject, applyPatchReference); err != nil {
+					return nil, nil, nil, fmt.Errorf("failed to merge patch step %d: %w", i, err)
+				}
+				fmt.Println("Merged patch reference:", applyObject)
+				continue
+			}
+		}
+		description, err := step.GetName()
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("failed to get name for step: %w", err)
+		}
+		chainsawStep := struct {
+			ChainsawStep `yaml:",inline"`
+			Description  string `yaml:"description,omitempty"`
+		}{
+			ChainsawStep: step.Chainsaw,
+			Description:  description,
+		}
+		chainsawSteps = append(chainsawSteps, chainsawStep)
+	}
+	return chainsawSteps, applyObject, applyObjectKeywords, nil
+}
+
+// Rule represents a Gherkin rule grouping related scenarios with modifications.
+type Rule struct {
+	ScenarioWrapper GherkinScenarioWrapper `json:",inline" yaml:",inline"`
+}
+
+//type Fixture struct {
+//	Permutations [][]map[string]any `yaml:"permutations,omitempty"`
+//	Combinations []map[string]any   `yaml:"combinations,omitempty"`
+//}
+//
+//// GetAll returns a list of all combinations and permutations defined in the fixture.
+//func (f *Fixture) GetAll() []map[string]any {
+//	var results []map[string]any
+//
+//	// Compute permutation combinations if any are defined.
+//	if len(f.Permutations) > 0 {
+//		perms := computePermutations(f.Permutations)
+//		results = append(results, perms...)
+//	}
+//
+//	// Add explicit combinations.
+//	if len(f.Combinations) > 0 {
+//		results = append(results, f.Combinations...)
+//	}
+//
+//	return results
+//}
+//
+//// computePermutations takes a slice of permutation groups and returns the Cartesian product
+//// of all groups as a slice of merged maps.
+//func computePermutations(groups [][]map[string]any) []map[string]any {
+//	// Start with an initial empty combination.
+//	result := []map[string]any{make(map[string]any)}
+//
+//	// For each group, build new combinations by merging each option with the existing base.
+//	for _, group := range groups {
+//		var newResult []map[string]any
+//		for _, base := range result {
+//			for _, option := range group {
+//				// Copy the base combination.
+//				merged := copyMap(base)
+//				// Merge the option into the base. In case of key conflict, option's value overwrites.
+//				for k, v := range option {
+//					merged[k] = v
+//				}
+//				newResult = append(newResult, merged)
+//			}
+//		}
+//		result = newResult
+//	}
+//
+//	return result
+//}
+//
+//// copyMap creates a shallow copy of the given map.
+//func copyMap(src map[string]any) map[string]any {
+//	dst := make(map[string]any)
+//	for k, v := range src {
+//		dst[k] = v
+//	}
+//	return dst
+//}
