49b03059a63f968fd5ca51c796fc7a89b0cfd44f
AV Suni <anttsuni@amd.com>
2025-04-26 21:22:49 +0300

    squash part 12

--- workloads/inference/LLMs/online-inference/vllm-online-single-multinode/__init__.py
+++ workloads/inference/LLMs/online-inference/vllm-online-single-multinode/__init__.py
@@ -132,10 +132,4 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     )
 
 
-deployment = build_app(
-    {
-        "model": os.environ["MODEL_ID"],
-        "tensor-parallel-size": os.environ["TENSOR_PARALLELISM"],
-        "pipeline-parallel-size": os.environ["PIPELINE_PARALLELISM"],
-    }
-)
+deployment = build_app()
