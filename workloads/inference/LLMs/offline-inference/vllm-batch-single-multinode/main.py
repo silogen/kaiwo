968ce36fbefc2ef60512d6bccf11ad054182ee78
Antti-Ville Suni <anttsuni@amd.com>
2025-04-28 17:19:09 +0300

    squash part 19

--- workloads/inference/LLMs/offline-inference/vllm-batch-single-multinode/main.py
+++ workloads/inference/LLMs/offline-inference/vllm-batch-single-multinode/main.py
@@ -14,97 +14,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-This example shows how to use Ray Data for data parallel batch inference.
-
-Ray Data is a data processing framework that can handle large datasets
-and integrates tightly with vLLM for data-parallel inference.
-
-As of Ray 2.44, Ray Data has a native integration with
-vLLM (under ray.data.llm).
-
-Ray Data provides functionality for:
-* Reading and writing to cloud storage (S3, GCS, etc.)
-* Automatic sharding and load-balancing across a cluster
-* Optimized configuration of vLLM using continuous batching
-* Compatible with tensor/pipeline parallel inference as well.
-
-Learn more about Ray Data's LLM integration:
-https://docs.ray.io/en/latest/data/working-with-llms.html
-"""
 import os
+from typing import Any, Dict, List
 
+import numpy as np
 import ray
-from packaging.version import Version
-from ray.data.llm import build_llm_processor, vLLMEngineProcessorConfig
 
-assert Version(ray.__version__) >= Version("2.44.1"), "Ray version must be at least 2.44.1"
+# from packaging.version import Version
+from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
+from vllm import LLM, SamplingParams
 
-# Uncomment to reduce clutter in stdout
-# ray.init(log_to_driver=False)
-# ray.data.DataContext.get_current().enable_progress_bars = False
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+num_instances = int(os.getenv("NUM_REPLICAS", "1"))
+tensor_parallel_size = int(os.getenv("NUM_GPUS_PER_REPLICA", "8"))
+
+
+class LLMPredictor:
+
+    def __init__(self):
+        self.llm = LLM(
+            model=os.getenv("MODEL_ID", "meta-llama/Llama-3.1-8B-Instruct"),
+            device="cuda",
+            tensor_parallel_size=tensor_parallel_size,
+            tokenizer_pool_size=4,
+            tokenizer_pool_type="ray",
+            distributed_executor_backend="ray",
+            trust_remote_code=True,
+        )
+
+    def __call__(self, batch: Dict[str, np.ndarray]) -> Dict[str, list]:
+        outputs = self.llm.generate(batch["text"], sampling_params)
+        prompt: List[str] = []
+        generated_text: List[str] = []
+        for output in outputs:
+            prompt.append(output.prompt)
+            generated_text.append(" ".join([o.text for o in output.outputs]))
+        return {
+            "prompt": prompt,
+            "generated_text": generated_text,
+        }
+
 
 # Read one text file from S3. Ray Data supports reading multiple files
 # from cloud storage (such as JSONL, Parquet, CSV, binary format).
 ds = ray.data.read_text("s3://anonymous@air-example-data/prompts.txt")
-print(ds.schema())
 
-size = ds.count()
-print(f"Size of dataset: {size} prompts")
 
-# Configure vLLM engine.
-config = vLLMEngineProcessorConfig(
-    model_source=os.getenv("MODEL_ID", "meta-llama/Llama-3.1-8B-Instruct"),
-    engine_kwargs={
-        "enable_chunked_prefill": True,
-        "max_num_batched_tokens": 4096,
-        "max_model_len": 16384,
-        "tensor_parallel_size": int(os.getenv("NUM_GPUS_PER_REPLICA", "8")),
-        "distributed_executor_backend": "ray",
-        "tokenizer_pool_size": 4,
-        "tokenizer_pool_type": "ray",
-        "trust_remote_code": True,
-        "device": "cuda",
-        "enforce_eager": False,
-    },
-    concurrency=int(os.getenv("NUM_REPLICAS", "1")),  # set the number of parallel vLLM replicas
-    batch_size=64,
+# For tensor_parallel_size > 1, we need to create placement groups for vLLM
+# to use. Every actor has to have its own placement group.
+def scheduling_strategy_fn():
+    # One bundle per tensor parallel worker
+    pg = ray.util.placement_group(
+        [{"GPU": 1, "CPU": 1}] * tensor_parallel_size,
+        strategy="PACK",
+    )
+    return dict(scheduling_strategy=PlacementGroupSchedulingStrategy(pg, placement_group_capture_child_tasks=True))
+
+
+resources_kwarg: Dict[str, Any] = {}
+if tensor_parallel_size == 1:
+    # For tensor_parallel_size == 1, we simply set num_gpus=1.
+    resources_kwarg["num_gpus"] = 1
+else:
+    # Otherwise, we have to set num_gpus=0 and provide
+    # a function that will create a placement group for
+    # each instance.
+    resources_kwarg["num_gpus"] = 0
+    resources_kwarg["ray_remote_args_fn"] = scheduling_strategy_fn
+
+ds = ds.map_batches(
+    LLMPredictor,
+    concurrency=num_instances,
+    # Specify the batch size for inference.
+    batch_size=32,
+    **resources_kwarg,
 )
 
-# Create a Processor object, which will be used to
-# do batch inference on the dataset
-vllm_processor = build_llm_processor(
-    config,
-    preprocess=lambda row: dict(
-        messages=[
-            {"role": "system", "content": "You are a bot that responds with haikus."},
-            {"role": "user", "content": row["text"]},
-        ],
-        sampling_params=dict(
-            temperature=0.3,
-            max_tokens=250,
-        ),
-    ),
-    postprocess=lambda row: dict(
-        answer=row["generated_text"], **row  # This will return all the original columns in the dataset.
-    ),
-)
-
-ds = vllm_processor(ds)
-
-# Peek first 10 results.
-# NOTE: This is for local testing and debugging. For production use case,
-# one should write full result out as shown below.
 outputs = ds.take(limit=10)
-
 for output in outputs:
     prompt = output["prompt"]
     generated_text = output["generated_text"]
-    print(f"Prompt: {prompt!r}")
-    print(f"Generated text: {generated_text!r}")
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
-# Write inference output data out as Parquet files to S3.
-# Multiple files would be written to the output destination,
-# and each task would write one or more files separately.
-#
 # ds.write_parquet("s3://<your-output-bucket>")
