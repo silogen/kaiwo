e609ece1e1383cf1d441310f0d95c0dfcd7f6deb
bjorn-amd <bjorn.ahlstrom@amd.com>
2025-10-17 11:32:11 +0200

    Generate rbac role.yaml, and add some missing kubebuilder markers for rbac

--- internal/controller/aim/aimservicetemplate_controller.go
+++ internal/controller/aim/aimservicetemplate_controller.go
@@ -70,6 +70,7 @@ type AIMServiceTemplateReconciler struct {
 // +kubebuilder:rbac:groups=serving.kserve.io,resources=servingruntimes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
 
 func (r *AIMServiceTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
