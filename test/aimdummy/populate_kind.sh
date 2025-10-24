2c19507ef522eb3cb786c1bec040ad7ec333978d
bjorn-amd <bjorn.ahlstrom@amd.com>
2025-10-24 09:59:08 +0200

    Consistent naming

--- test/aimdummy/populate_kind.sh
+++ test/aimdummy/populate_kind.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
-docker build -t aimfaker:dev-latest .
-kind load docker-image aimfaker:dev-latest --name kaiwo-test
+docker build -t aimdummy:dev-latest .
+kind load docker-image aimdummy:dev-latest --name kaiwo-test
