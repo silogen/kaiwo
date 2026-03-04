fa7e7ea5dccfb17cc53662f554ae0ce472c3ffae
Alex Saliniemi <alex.saliniemi@amd.com>
2026-03-04 11:19:22 +0000

    update rbac markers for aimservices

--- internal/controller/gpuworkload_controller.go
+++ internal/controller/gpuworkload_controller.go
@@ -98,6 +98,8 @@ func IsGpuPreemptionEnabled() bool {
 // +kubebuilder:rbac:groups=ray.io,resources=rayjobs;rayservices;rayclusters,verbs=get;list;delete
 // +kubebuilder:rbac:groups=workload.codeflare.dev,resources=appwrappers,verbs=get;list;delete
 // +kubebuilder:rbac:groups=kaiwo.silogen.ai,resources=kaiwojobs;kaiwoservices,verbs=get;list;delete
+// +kubebuilder:rbac:groups=aim.silogen.ai,resources=aimservices,verbs=get;list;delete
+// +kubebuilder:rbac:groups=aim.eai.amd.com,resources=aimservices,verbs=get;list;delete
 // +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;watch;create;update
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
